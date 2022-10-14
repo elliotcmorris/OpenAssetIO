984fc7ee921d5b07e5c96c8b74a95e33311de2e5
David Feltell <david.feltell@foundry.com>
2022-10-14 17:32:48 +0100

    [Build] Add setup.py
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- setup.py
+++ setup.py
@@ -27,8 +27,23 @@ import setuptools.command.build_ext
 
 
 class build_ext(setuptools.command.build_ext.build_ext):
+    """
+    Custom setuptools command class to puppet CMake.
+    """
+
     def build_extension(self, _ext: Extension):
-        self._cmake(
+        """
+        Hook called by setuptools to build the given Extension.
+
+        Note: For develop installs to work with binary artifacts we must
+        use "strict" editable installs, i.e. use
+        `pip install --editable --config-settings editable_mode=strict`
+        https://setuptools.pypa.io/en/latest/userguide/development_mode.html
+
+        @param _ext: Extension to build (we only have one, so ignored).
+        @return:
+        """
+        self.__cmake(
             [
                 "-S",
                 ".",
@@ -37,25 +52,16 @@ class build_ext(setuptools.command.build_ext.build_ext):
                 "-G",
                 "Ninja",
                 # Place output artifacts where setuptools expects.
-                f"-DCMAKE_INSTALL_PREFIX={self.build_lib}",
-                # Build a single DSO that combines the core library with
-                # Python bindings.
-                "-DBUILD_SHARED_LIBS=OFF",
-                # Install to root of install tree rather than
-                # `lib/.../site-packages`, since that is what setuptools
-                # expects.
-                "-DOPENASSETIO_PYTHON_SITEDIR=.",
-                # TODO(DF): This is required if running from a
-                # venv in order to use the external (conan)
-                # Python interpreter/headers. Probably not what
-                # we want for deployment.
-                "-DPython_FIND_VIRTUALENV=STANDARD",
+                "--install-prefix",
+                os.path.abspath(self.build_lib),
+                "--preset",
+                "pip",
             ]
         )
 
-        self._cmake(["--build", self.build_temp, "--target", "openassetio-python-module"])
+        self.__cmake(["--build", self.build_temp, "--target", "openassetio-python-module"])
 
-        self._cmake(
+        self.__cmake(
             [
                 "--install",
                 self.build_temp,
@@ -64,15 +70,53 @@ class build_ext(setuptools.command.build_ext.build_ext):
             ]
         )
 
-    def _cmake(self, args: list[str]):
+    def __cmake(self, args: list[str]):
+        """
+        Execute `cmake` as a subprocess with given arguments.
+
+        @param args: Command-line arguments to pass to `cmake`.
+        """
         args = ["cmake"] + args
         self.announce(" ".join(map(shlex.quote, args)), level=2)
         subprocess.check_call(args, env=os.environ.copy(), stderr=subprocess.STDOUT)
 
     def get_outputs(self):
+        """
+        Get list of output artifacts created by this command.
+
+        For develop (`--editable`) installs, this is used to get the
+        list of files to link to or copy from.
+
+        We must override the base class because the default
+        implementation uses `get_output_mapping`, which we override
+        to return an empty dict (see below).
+
+        @see get_output_mapping
+        @return: List of output files.
+        """
         return dict(self._get_output_mapping()).keys()
 
     def get_output_mapping(self):
+        """
+        Get the mapping from build artifacts back to original source
+        files.
+
+        Since build artifacts are generated (i.e. compiled from C++),
+        there are no source files to link to, so the mapping is empty.
+
+        For develop (`--editable`) installs, this is used to determine
+        whether a file can be linked back to the source directory, or
+        must be copied. Ensuring we return a blank mapping means the
+        output artifacts are always copied.
+
+        We must override this from the base class since the default
+        implementation generates a mapping expecting the source tree to
+        contain the binary artifacts pre-built at the root of the
+        package.
+
+        @return: Mapping of build artifacts to corresponding source
+        artifact (i.e. blank in this case).
+        """
         return {}
 
 
