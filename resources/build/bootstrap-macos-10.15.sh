48ca7ce89fcafc93f63b040b10709720707ac327
Elliot Morris <elliot.morris@foundry.com>
2022-10-21 15:01:14 +0100

    [Build] Add CIBuildWheel Actions Pipeline
    Part of #653
    
    In order to have an automated release pipeline to provide prebuilt
    wheels to PyPI, establish an automated pipeline that builds python
    wheels across multiple platforms/python versions, using cibuildwheel.
    
    As of this commit,  targets python3.9 and python3.10, on Windows
    and Ubuntu, x86_64 only. (Matrix of 4).
    
    Pipeline will be invoked on every push to main branch.
    
    Implementation note : This commit includes a change to migrate the conan
    cmake toolchain file path from user home to the repository. This was
    done to provide consistency with non-CI build steps, as well as to allow
    cibuildwheel (or more correctly, CMake invoked under the docker
    container cibuildwheel uses on Linux platforms) to simply accept
    the file argument as a relative path.
    
    Signed-off-by: Elliot Morris <elliot.morris@foundry.com>

--- resources/build/bootstrap-macos-10.15.sh
+++ resources/build/bootstrap-macos-10.15.sh
@@ -11,7 +11,22 @@ export CONAN_USER_HOME="$HOME/conan"
 # Create default conan profile so we can configure it before install.
 # Use --force so that if it already exists we don't error out.
 conan profile new default --detect --force
+
+# In the case that this script is run from an environment (cibuildwheel)
+# that already has python, don't install over them as they'll conflict.
+# We also don't need to install the test dependencies in that case, as
+# they're unused in the release workflow.
+INSTALL_PYTHON=True
+REQUIRE_TEST_DEPS=True
+if [ "$CIBUILDWHEEL" = "1" ]
+then
+  INSTALL_PYTHON=False
+  REQUIRE_TEST_DEPS=False
+fi
+
 # Install openassetio third-party dependencies from public Conan Center
 # package repo.
 conan install --install-folder "$WORKSPACE/.conan" --build=missing \
+    -o testing=$REQUIRE_TEST_DEPS \
+    -o install_cpython=$INSTALL_PYTHON \
     "$WORKSPACE/resources/build"
--- resources/build/bootstrap-windows-2019.bat
+++ resources/build/bootstrap-windows-2019.bat
@@ -7,7 +7,21 @@ set CONAN_USER_HOME=%USERPROFILE%\conan
 :: Use --force so that if it already exists we don't error out.
 conan profile new default --detect --force
 conan profile show default
+
+:: In the case that this script is run from an environment (cibuildwheel)
+:: that already has python, don't install over them as they'll conflict.
+:: We also don't need to install the test dependencies in that case, as
+:: they're unused in the release workflow.
+set INSTALL_PYTHON=True
+set REQUIRE_TEST_DEPS=True
+if "%CIBUILDWHEEL%"=="1" ( 
+  set INSTALL_PYTHON=False
+  set REQUIRE_TEST_DEPS=False
+)
+
 :: Install openassetio third-party dependencies from public Conan Center
 :: package repo.
 conan install --install-folder "%WORKSPACE%\.conan" ^
-              --build=missing "%WORKSPACE%\resources\build"
+              --build=missing "%WORKSPACE%\resources\build" ^
+              -o testing=%INSTALL_PYTHON% ^
+              -o install_cpython=%REQUIRE_TEST_DEPS%
