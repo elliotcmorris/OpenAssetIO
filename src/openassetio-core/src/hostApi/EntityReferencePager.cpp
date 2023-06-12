c31f81776ed2d6dadcd071df602c0098d511b39f
Elliot Morris <elliot.morris@foundry.com>
2023-06-12 17:19:59 +0100

    [Core] Add EntityReferencePager object
    Add the EntityReferencePager and EntityReferencePagerInterface types,
    to support interfaces that may return large datasets, specifically
    RelatedReferences
    
    EntityReferencePager and EntityReferencePagerInterface follow a similar
    pattern to Manager and ManagerInterface, with PagerInterface intended as
    a base interface for a manager to implement, with Pager being the object
    that the host constructs.
    
    Signed-off-by: Elliot Morris <elliot.morris@foundry.com>

---
+++ src/openassetio-core/src/hostApi/EntityReferencePager.cpp
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2013-2023 The Foundry Visionmongers Ltd
+
+#include <openassetio/EntityReference.hpp>
+#include <openassetio/hostApi/EntityReferencePager.hpp>
+#include <openassetio/managerApi/EntityReferencePagerInterface.hpp>
+
+namespace openassetio {
+inline namespace OPENASSETIO_CORE_ABI_VERSION {
+namespace hostApi {
+
+typename EntityReferencePager::Ptr EntityReferencePager::make(
+    managerApi::EntityReferencePagerInterfacePtr pagerInterface,
+    managerApi::HostSessionPtr hostSession) {
+  // Trick to give private constructor access to std::make_shared.
+  struct Constructible : EntityReferencePager {
+    Constructible(managerApi::EntityReferencePagerInterfacePtr pagerInterface,
+                  managerApi::HostSessionPtr hostSession)
+        : EntityReferencePager(std::move(pagerInterface), std::move(hostSession)) {}
+  };
+
+  return std::make_shared<Constructible>(std::move(pagerInterface), std::move(hostSession));
+}
+
+EntityReferencePager::EntityReferencePager(
+    managerApi::EntityReferencePagerInterfacePtr pagerInterface,
+    managerApi::HostSessionPtr hostSession)
+    : pagerInterface_(std::move(pagerInterface)), hostSession_(std::move(hostSession)) {}
+
+bool EntityReferencePager::hasNext() { return pagerInterface_->hasNext(hostSession_); }
+
+typename EntityReferencePager::Page EntityReferencePager::get() {
+  return pagerInterface_->get(hostSession_);
+}
+
+void EntityReferencePager::next() { pagerInterface_->next(hostSession_); }
+
+}  // namespace hostApi
+}  // namespace OPENASSETIO_CORE_ABI_VERSION
+}  // namespace openassetio
---
+++ src/openassetio-core/tests/hostApi/EntityReferencePagerTest.cpp
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2022 The Foundry Visionmongers Ltd
+#include <type_traits>
+#include <variant>
+
+#include <openassetio/export.h>
+
+#include <catch2/catch.hpp>
+#include <catch2/trompeloeil.hpp>
+
+#include <openassetio/hostApi/HostInterface.hpp>
+#include <openassetio/log/LoggerInterface.hpp>
+#include <openassetio/managerApi/Host.hpp>
+#include <openassetio/managerApi/HostSession.hpp>
+#include <openassetio/managerApi/ManagerInterface.hpp>
+
+#include <openassetio/hostApi/EntityReferencePager.hpp>
+#include <openassetio/managerApi/EntityReferencePagerInterface.hpp>
+namespace openassetio {
+inline namespace OPENASSETIO_CORE_ABI_VERSION {
+namespace {
+/**
+ * Mock implementation of a ManagerInterface.
+ *
+ * Used as constructor parameter to the Manager under test.
+ */
+struct MockManagerInterface : trompeloeil::mock_interface<managerApi::ManagerInterface> {
+  IMPLEMENT_CONST_MOCK0(identifier);
+  IMPLEMENT_CONST_MOCK0(displayName);
+  IMPLEMENT_CONST_MOCK0(info);
+  IMPLEMENT_MOCK2(initialize);
+  IMPLEMENT_CONST_MOCK3(managementPolicy);
+  IMPLEMENT_CONST_MOCK2(isEntityReferenceString);
+  IMPLEMENT_MOCK6(resolve);
+  IMPLEMENT_MOCK6(preflight);
+  IMPLEMENT_MOCK6(register_);  // NOLINT(readability-identifier-naming)
+};
+/**
+ * Mock implementation of a HostInterface.
+ *
+ * Used as constructor parameter to Host classes required as part of these tests
+ */
+struct MockHostInterface : trompeloeil::mock_interface<hostApi::HostInterface> {
+  IMPLEMENT_CONST_MOCK0(identifier);
+  IMPLEMENT_CONST_MOCK0(displayName);
+  IMPLEMENT_CONST_MOCK0(info);
+};
+/**
+ * Mock implementation of a LoggerInterface
+ *
+ * Used as constructor parameter to Host classes required as part of these tests
+ */
+struct MockLoggerInterface : trompeloeil::mock_interface<log::LoggerInterface> {
+  IMPLEMENT_MOCK2(log);
+};
+
+/**
+ * Fixture providing a Manager instance injected with mock dependencies.
+ */
+struct ManagerFixture {
+  const std::shared_ptr<managerApi::ManagerInterface> managerInterface =
+      std::make_shared<openassetio::MockManagerInterface>();
+
+  // For convenience, to avoid casting all the time in tests.
+  MockManagerInterface& mockManagerInterface =
+      static_cast<openassetio::MockManagerInterface&>(*managerInterface);
+
+  // Create a HostSession with our mock HostInterface
+  const managerApi::HostSessionPtr hostSession = managerApi::HostSession::make(
+      managerApi::Host::make(std::make_shared<openassetio::MockHostInterface>()),
+      std::make_shared<openassetio::MockLoggerInterface>());
+};
+
+}  // namespace
+}  // namespace OPENASSETIO_CORE_ABI_VERSION
+}  // namespace openassetio
+
+namespace {
+struct MockEntityReferencePagerInterface
+    : trompeloeil::mock_interface<openassetio::managerApi::EntityReferencePagerInterface> {
+  IMPLEMENT_MOCK1(hasNext);
+  IMPLEMENT_MOCK1(get);
+  IMPLEMENT_MOCK1(next);
+};
+
+}  // namespace
+
+SCENARIO("Using an EntityReferencePager") {
+  using trompeloeil::_;
+
+  GIVEN("a configured EntityReferencePager") {
+    const openassetio::trait::TraitSet traits = {"fakeTrait", "secondFakeTrait"};
+    const openassetio::ManagerFixture fixture;
+
+    const std::shared_ptr<MockEntityReferencePagerInterface> mockEntityReferencePagerInterface =
+        std::make_shared<MockEntityReferencePagerInterface>();
+
+    const openassetio::hostApi::EntityReferencePager::Ptr pager =
+        openassetio::hostApi::EntityReferencePager::make(mockEntityReferencePagerInterface,
+                                                         fixture.hostSession);
+
+    AND_GIVEN("plugin's hasMore expects to be called and returns false") {
+      REQUIRE_CALL(*mockEntityReferencePagerInterface, hasNext(fixture.hostSession)).RETURN(false);
+      WHEN("host wrapper's hasMore is called") {
+        const bool hasMoreReturn = pager->hasNext();
+        THEN("the value is false") { CHECK(hasMoreReturn == false); }
+      }
+    }
+    AND_GIVEN("plugin's hasMore expects to be called and returns true") {
+      REQUIRE_CALL(*mockEntityReferencePagerInterface, hasNext(fixture.hostSession)).RETURN(true);
+      WHEN("host wrapper's hasMore is called") {
+        const bool hasMoreReturn = pager->hasNext();
+        THEN("the value is true") { CHECK(hasMoreReturn == true); }
+      }
+    }
+    AND_GIVEN("plugin's next expects to be called") {
+      REQUIRE_CALL(*mockEntityReferencePagerInterface, next(fixture.hostSession));
+      WHEN("host wrapper's next is called") {
+        pager->next();
+        THEN("pagerInterface next was called") {}
+      }
+    }
+    AND_GIVEN("plugin's get expects to be called and returns some entity references") {
+      const std::vector<openassetio::EntityReference> testEntRefs = {
+          openassetio::EntityReference("One!"), openassetio::EntityReference("Two!")};
+      REQUIRE_CALL(*mockEntityReferencePagerInterface, get(fixture.hostSession))
+          .RETURN(testEntRefs);
+      WHEN("host wrapper's get is called") {
+        const openassetio::EntityReferences getReturn = pager->get();
+        THEN("the returned entity references match those provided by the plugin") {
+          CHECK(getReturn == testEntRefs);
+        }
+      }
+    }
+  }
+}
